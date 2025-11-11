e338c8766db931f5b4da1970d219b3702ad14520
Koji Watanabe <kojwatan@f2r5s8.clusters.42paris.fr>
2025-11-11 11:37:36 +0100

    hotfix: type

--- ex04/ft_calculator.py
+++ ex04/ft_calculator.py
@@ -4,7 +4,7 @@ class calculator:
     @staticmethod
     def dotproduct(V1: list[float], V2: list[float]) -> None:
         """Return the dot product of two vectors."""
-        ret: float = 0.0
+        ret = 0
         for i in range(len(V1)):
             ret += V1[i] * V2[i]
         print(f"Dot product is: {ret}")
